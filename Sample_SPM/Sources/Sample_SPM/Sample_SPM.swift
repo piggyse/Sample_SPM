88eba98cef401a78f0c37b0118583934e1071bf5
soby.jin <soby.axz@axzcorp.com>
2025-09-07 15:05:33 +0900

    init project

---
+++ Sample_SPM/Sources/Sample_SPM/Sample_SPM.swift
@@ -0,0 +1,13 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+public struct Sample_SPM_Pubic_Sample_Struct {
+    
+    public let propertyA: String
+    public let PropertyB: String
+    
+    public init(propertyA: String, PropertyB: String) {
+        self.propertyA = propertyA
+        self.PropertyB = PropertyB
+    }
+}
