562a26e226db4e899f0e33830fbe235f1204cbe0
piggyse <iospiggy95@gmail.com>
2025-09-07 15:53:50 +0900

    오타 수정

--- Sources/Sample_SPM/Sample_SPM.swift
+++ Sources/Sample_SPM/Sample_SPM.swift
@@ -1,13 +1,13 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-public struct Sample_SPM_Pubic_Sample_Struct {
+public struct Sample_SPM_Public_Sample_Struct {
     
     public let propertyA: String
-    public let PropertyB: String
+    public let propertyB: String
     
-    public init(propertyA: String, PropertyB: String) {
+    public init(propertyA: String, propertyB: String) {
         self.propertyA = propertyA
-        self.PropertyB = PropertyB
+        self.propertyB = propertyB
     }
 }
